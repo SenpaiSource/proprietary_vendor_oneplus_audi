221195e45c35cfe8c75e4ef7a6780d20729fa7b8
SathiyaSenpai <25bai042@hicas.ac.in>
2026-08-29 03:19:24 +0530

    audi: Update from COS 11.F.15

---
+++ Android.mk
@@ -0,0 +1,35 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),audi)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,8f85ade193af580367082389f0b4676e0d96ddf8)
+$(call add-radio-file-sha1-checked,radio/aop.img,5da26126914721e057886fac9d6cef92270dde68)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,d49e76c3a9f1f402d8bf4dbbc18750f37a547d29)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,4f4c1e37a208642035e20c2e869644d02e016556)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,67261a3d7435f34cfaecf4877989202804b937ee)
+$(call add-radio-file-sha1-checked,radio/cpucp_dtb.img,fe52c989d863c46c30d18071c881c5bf12241702)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,7ad775e9291a8ab04d4200b83feaa6e4609e15e8)
+$(call add-radio-file-sha1-checked,radio/dsp.img,7e81146e07bb4120616a084cc932dc4836420d29)
+$(call add-radio-file-sha1-checked,radio/engineering_cdt.img,6aca5306512a67b0eb15a3d0fe93a5ecd176652d)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,007fa454d5c62dab041cc96ecd99eab474a4eb9c)
+$(call add-radio-file-sha1-checked,radio/hyp.img,b168c044a819acc37df127a591a00ba018febd3d)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,fa14740c79c2f5c99f2549190d5165d9582a81fe)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,b432062db66083b63377c71f188825e4b2edab71)
+$(call add-radio-file-sha1-checked,radio/modem.img,c27ab301568409bff4401e41495f923b22dfa2ea)
+$(call add-radio-file-sha1-checked,radio/oplus_sec.img,fb7d5b89feac2ef035e98e945d5f094e8d1ed95c)
+$(call add-radio-file-sha1-checked,radio/oplusstanvbk.img,de6876d1bd060166a5baf04bc1aa37c122fb5ca2)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,28bb069571e0dcc7aad1c5827af3964460197c66)
+$(call add-radio-file-sha1-checked,radio/shrm.img,425f7f96f453b08bf07c66ca051352e794491454)
+$(call add-radio-file-sha1-checked,radio/splash.img,fc5da5e9d4d388820d09510b3ed2ecb895fef5fe)
+$(call add-radio-file-sha1-checked,radio/tz.img,acb9c68324077118d5e349c5c0db1003774c093e)
+$(call add-radio-file-sha1-checked,radio/uefi.img,ca1e123477766753bce125d02af6fbfc7b7978ef)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,4da4cd270fc4e802447c46006495fa739481adfa)
+$(call add-radio-file-sha1-checked,radio/xbl.img,6edf252acb52c0a3fca30968a66929abba3e668f)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,af534c3f43c8cbd65888cc54017a6b34c4926247)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,80cbd1dc6057d8e666329a610085cb3bf6c6eb85)
+
+endif
